ebd87826fe9b48786daa6331e6c84425eeb26c00
Sonhoccode <magonsilver05@gmail.com>
2024-11-11 18:57:27 +0700

    hson

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lập trình web</title>
+    <link rel="icon" type="x-icon" href="./pages/hình/icon/x-icon.png">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://kit.fontawesome.com/8ae1e04fc1.js" crossorigin="anonymous"></script>
+    icon
 </head>
 
 <body>
